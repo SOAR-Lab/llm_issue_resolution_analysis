73444305ec4cd5184986734a6cad02df88be491b
ramtin-ehsani <ramtin.ehsani79@gmail.com>
2024-04-05 12:33:22 -0400

    added utils

---
+++ conversation_analysis/utils/api_call.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class ApiCallStyle(Enum):
+    ELMB = r'[a-zA-Z]\w* [\(\w]'
+    ELMG = r'[a-zA-Z]\w* [\(\w]'
+    PYTHON = r'[a-zA-Z]\w*\s*\('
+    CLOJURE = r'\([a-zA-Z]\w* [a-zA-Z_]\w*'
+    RACKET = r'\([a-zA-Z]\w* [a-zA-Z_]\w*'
+    JAVA = r'[a-zA-Z]\w*\.'  # e.g., functionName(argument)
+    JAVASCRIPT = r'[a-zA-Z]\w*\('  # e.g., functionName(argument)
+    CSHARP = r'[a-zA-Z]\w*\('  # e.g., functionName(argument)
+    SWIFT = r'[a-zA-Z]\w*\('  # e.g., functionName(argument)
+
+    def __repr__(self):
+        return self.name
